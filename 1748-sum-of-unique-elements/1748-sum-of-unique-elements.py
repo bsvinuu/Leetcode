dcc0e145431920f61bd37abcf6c536507437358a
BS.Vignesh <158137778+bsvinuu@users.noreply.github.com>
2026-07-04 19:08:59 +0530

    Time: 0 ms (100%), Space: 19 MB (99.64%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
@@ -1,7 +1,26 @@
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
-        temp_list=list(map(lambda x : x if nums.count(x)==1 else 0,nums))
-        return sum(temp_list)
+        'solution one liner but time complexicity is high because of count'
+        # temp_list=list(map(lambda x : x if nums.count(x)==1 else 0,nums))
+        # return sum(temp_list)
+        'this is get the freq count and filter the elements and extract the values'
+        freq_dic={}
+        for value in nums:
+            if value not in freq_dic:
+                        freq_dic[value]=1
+            else:
+                temp_value=freq_dic.get(value)
+                temp_value+=1
+                freq_dic[value]=temp_value
+        final_output=[]
+        for key,value in freq_dic.items():
+            if value==1:
+                    final_output.append(key)
+
+        return sum(final_output)  
+
+
+
 
 
         
