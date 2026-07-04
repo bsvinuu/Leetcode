53b1f15cf877730c7e99dbfd3bf9fcd7c768e074
BS.Vignesh <158137778+bsvinuu@users.noreply.github.com>
2026-07-04 18:38:07 +0530

    Time: 9 ms (19.73%), Space: 19.3 MB (19.27%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,27 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        mapping_dict = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+        total_len=len(list(map(lambda x:x,s)))
+        output=0
+        for index,symbol in enumerate(s):
+            if total_len-1 !=index:
+                # print(index)
+                if symbol=='I' and s[index+1] in ('V','X'):
+                    output-=mapping_dict[symbol]
+                elif symbol=='X' and s[index+1] in ('L','C'):
+                    output-=mapping_dict[symbol]
+                elif symbol=='C' and s[index+1] in ('D','M'):
+                    output-=mapping_dict[symbol]
+                else:
+                    output+=mapping_dict[symbol]
+            else:
+                output+=mapping_dict[symbol]              
+        return output
