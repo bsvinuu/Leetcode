2f1eb86f1c6197728c19f784099e3f574a1db823
BS.Vignesh <158137778+bsvinuu@users.noreply.github.com>
2026-07-13 09:30:01 +0530

    Time: 119 ms (9.94%), Space: 19.4 MB (36.93%) - LeetHub

--- 3364-minimum-positive-sum-subarray/3364-minimum-positive-sum-subarray.py
+++ 3364-minimum-positive-sum-subarray/3364-minimum-positive-sum-subarray.py
@@ -4,11 +4,8 @@ class Solution:
         for window_size in range(l,r+1):
             left,right=0,window_size
             while left < len(nums) and right <=len(nums) :
-                # print(nums[left:right],sum(nums[left:right]))
-                # print(left,right)
                 if sum(nums[left:right])>0 and  sum(nums[left:right]) < min_subarray_sum:
                     min_subarray_sum=min(min_subarray_sum,sum(nums[left:right]))
-                    # output.append(list(nums[left:right]))
                 right+=1
                 left=left+1
         if min_subarray_sum == float('inf'):
